d65df4886fc1e9a1b6684e9793d0e8f2a702f6b2
amit123ak <41484488+amit123ak@users.noreply.github.com>
2025-03-30 13:27:20 +0530

    Create PrintedEveryCharTwice.java
    Printed Every Char Twice

---
+++ PrintedEveryCharTwice.java
@@ -0,0 +1,38 @@
+package NewProjectTest.NewProjectTest;
+
+public class PrintedEveryCharTwice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        String str = "Java";
+        
+        PrintedEveryCharTwice(str);
+
+
+
+
+
+
+
+	}
+
+	public  static void PrintedEveryCharTwice(String str) {
+		// TODO Auto-generated method stub
+		
+ 
+	
+	for(int i=0;i<str.length();i++)
+	{
+		
+		System.out.print(str.charAt(i)+""+str.charAt(i));
+	}
+
+
+		
+		
+
+	}
+	
+
+}
